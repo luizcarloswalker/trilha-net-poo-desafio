23070717072572f08d8d979ea3cd703387fe280e
Luiz Carlos Walker <luizcarloswalker@gmail.com>
2023-10-24 17:11:50 -0300

    atualizacao de arquivos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+﻿﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone nokia: ");
+Smartphone nokia = new Nokia(numero:"50030266", modelo:"pro", imei:"1000101101", memoria:64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero:"12349874", modelo:"proMax", imei:"0110110110", memoria:128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
